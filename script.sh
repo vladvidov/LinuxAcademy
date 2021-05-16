b0e74dd10bfa88fc65a0ca3dbf655aa0a06e2552
Volodymyr Vydovskyi <vladvidov@gmail.com>
2021-05-16 20:35:12 +0300

    First commit. Added new script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is a script from github"
+echo "Here you can see an output of a paramter $parameter1"
+echo "And here we can see arithmetic operations"
+echo "Number $numberOne + number $numberTwo is $(( $numberOne + $numberTwo ))"
+echo "That's all thanks."
+echo "Weeeeeeel"
